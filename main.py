b10c54dd3bda4192cdf6095d3c8cb6adf544c466
Victor Goncharov <victor.goncharov@gmail.com>
2023-03-18 01:57:37 +0100

    Version 2.0

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-import csv
+import subprocess
 import os
 
-from tabulate import tabulate
-from colorama import Fore, Back, Style
+def install_dependencies(pip_command):
+    if os.path.isfile('dep_inst.txt'): # let's check if dependancies are already installed
+        return
+    
+    try:
+        print("\033[3;2mInstalling dependencies...\033[0m", end=" ")
+        subprocess.check_call([pip_command, "install", "-r", "requirements.txt", "--disable-pip-version-check"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        print("\033[3;2mDependencies installed successfully!\033[0m")
+        with open('dep_inst.txt', 'w') as f:
+            f.write('True') # write a flag to indicate that the dependencies are installed
+    except subprocess.CalledProcessError:
+        print("\033[3;31mFailed to install dependencies.\033[0m")
+
+def determine_pip_command():
+    """
+    Determine which version of pip to use.
+    """
+    devnull = open(os.devnull, 'w')
+    pip_command = None
+    if subprocess.call(["pip", "--version"], stdout=devnull, stderr=devnull) == 0:
+        pip_command = "pip"
+    elif subprocess.call(["pip3", "--version"], stdout=devnull, stderr=devnull) == 0:
+        pip_command = "pip3"
+    else:
+        print("\033[3;31mError: pip is not installed.\033[0m")
+        exit()
+    return pip_command
+
+#check the dependancies and install them
+install_dependencies(determine_pip_command())
+
+
+import csv
+import pandas as pd
+from termcolor import colored
+from tqdm import tqdm
+import openpyxl
+
 
 def calculate_elo_rating(winner, loser, winner_score, loser_score):
     K = 50  # constant factor to adjust the weight of each game
@@ -45,12 +81,21 @@ def update_players(players, team1, team2, score1, score2):
     players[loser1] = new_loser_elo[0] if new_loser_elo[0] > 0 else 0
     players[loser2] = new_loser_elo[1] if new_loser_elo[1] > 0 else 0
     
-    # Write point history of each player to CSV file
+    # Write point history of each player to the excel file
     for player in [winner1, winner2, loser1, loser2]:
-       filename = f"player_stats/{player}_history.csv"
-       with open(filename, "a", newline="") as csvfile:
-           writer = csv.writer(csvfile)
-           writer.writerow([players[player]]) 
+        filename = "player_stats/player_stats.xlsx"
+        sheetname = f"{player}"
+        try:
+            workbook = openpyxl.load_workbook(filename)
+        except FileNotFoundError:
+            workbook = openpyxl.Workbook()
+        if sheetname not in workbook.sheetnames:
+            workbook.create_sheet(sheetname)
+        sheet = workbook[sheetname]
+        sheet.append([players[player]])
+        workbook.save(filename)
+    
+    
     return players
 
 
@@ -63,36 +108,65 @@ def print_ranking(players):
     rows = []
     for i, (player, points) in enumerate(ranking, start=1):
         rank = f"{i}"
+        if i == 1:
+            player = f"*{player}"
         rows.append([rank, player, round(points)])
     
-    table = tabulate(rows, headers=headers, tablefmt="fancy_grid", numalign="center", stralign="center")
+    # Set the width of each column
+    col_widths = [6, 25, 8]
     
-    print(Fore.LIGHTGREEN_EX + Style.BRIGHT + table + Style.RESET_ALL)
+    # Print the headers
+    print(f"\n\033[1m{headers[0]:<{col_widths[0]}}{headers[1]:<{col_widths[1]}}{headers[2]:<{col_widths[2]}}\033[0m")
+    
+    # Print the separator
+    print("=" * sum(col_widths))
+    
+    # Print the rows
+    for row in rows:
+        print(f"{row[0]:<{col_widths[0]}}{row[1]:<{col_widths[1]}}{row[2]:<{col_widths[2]}}")
+    
+    # Create a pandas dataframe from the rankings
+    df = pd.DataFrame(rows, columns=headers)
+    print("\033[3;2m\nExporting to ranks.xlsx...\033[0m",end=" ")
+    # Write the dataframe to an Excel file
+    df.to_excel('ranks.xlsx', index=False)
+    print("\033[3;2mdone!\033[0m")
 
+
+def read_csv_file(file_path):
+    rows = []
+    with open(file_path, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(csvreader) # skip the first row
+        for row in csvreader:
+            if not all(row):  # skip rows with missing values
+                continue
+            rows.append(row)
+    return rows
 
 def main():
     players = {}  # initialize an empty dictionary to store player names and ELO ratings
 
 # Create directory if it doesn't exist
     if not os.path.exists('player_stats'):
-     os.makedirs('player_stats')
+        os.makedirs('player_stats')
+    if os.path.exists('player_stats/player_stats.xlsx'):
+        os.remove('player_stats/player_stats.xlsx') # delete old stats to prevent duplications
 
-    with open("scores.csv") as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader) # skip the first row
-        for row in reader:
-            if len(row) >= 6:
-                team1 = row[0], row[1]
-                team2 = row[2], row[3]
-                score1, score2 = int(row[4]), int(row[5])
-                for player in team1 + team2:
-                    if player not in players:
-                        players[player] = int(1000)  # initialize new player's score to 1000
-                players = update_players(players, team1, team2, score1, score2)
+    reader = read_csv_file('scores.csv')
+    for row in tqdm(reader, desc="Processing..."):
+        if len(row) >= 6:
+            team1 = row[0], row[1]
+            team2 = row[2], row[3]
+            score1, score2 = int(row[4]), int(row[5])
+            for player in team1 + team2:
+                if player not in players:
+                    players[player] = int(1000)  # initialize new player's score to 1000
+            players = update_players(players, team1, team2, score1, score2)
     
     print_ranking(players)
 
 
 # run the program
 if __name__ == "__main__":
-    main()
+    main() 
